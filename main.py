0d3e5f66c1447a21b037ddd0d60c84b973aebcbb
aimanafzal <ayman.afzal@msn.com>
2021-11-22 19:48:42 +0500

    Implementing query parameters

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from typing import Optional
+from enum import Enum
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id:str):
+    return {"user_id": user_id}
+    
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet  = "lenet"
+    
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
+    
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name":"Baz"}]
+@app.get("/items/")
+async def read_items(skip:int = 0, limit:int = 10):
+    return fake_items_db[skip:skip+limit]
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_uesr_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        return {"description": "A long description"}
+    return {item}
+
+@app.get("/items/{item_id}")
+async def read_user_id(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
